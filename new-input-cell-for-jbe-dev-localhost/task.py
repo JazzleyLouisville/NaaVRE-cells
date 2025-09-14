044b02b2c8957a556260e7ee479dffa4629ed739
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-14 17:47:37 +0200

    new-input-cell-for-jbe-dev-localhost creation

---
+++ new-input-cell-for-jbe-dev-localhost/task.py
@@ -0,0 +1,24 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+numbers = [1,2,3,4,5]
+
+file_numbers = open("/tmp/numbers_" + id + ".json", "w")
+file_numbers.write(json.dumps(numbers))
+file_numbers.close()
