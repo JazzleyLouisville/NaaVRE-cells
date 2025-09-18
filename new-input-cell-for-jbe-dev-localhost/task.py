c3c645689a770fab49c00dfe2e073a71447a8df8
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-18 18:32:16 +0200

    new-input-cell-for-jbe-dev-localhost update

--- new-input-cell-for-jbe-dev-localhost/task.py
+++ new-input-cell-for-jbe-dev-localhost/task.py
@@ -18,7 +18,11 @@ id = args.id
 
 
 numbers = ["x","y","z","a"]
+followers = [1,2,3,4,5]
 
 file_numbers = open("/tmp/numbers_" + id + ".json", "w")
 file_numbers.write(json.dumps(numbers))
 file_numbers.close()
+file_followers = open("/tmp/followers_" + id + ".json", "w")
+file_followers.write(json.dumps(followers))
+file_followers.close()
