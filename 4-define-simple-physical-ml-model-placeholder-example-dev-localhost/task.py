38800ba327e01435abbe526eec169e700e13b364
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-22 19:17:14 +0200

    4-define-simple-physical-ml-model-placeholder-example-dev-localhost creation

---
+++ 4-define-simple-physical-ml-model-placeholder-example-dev-localhost/task.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--cleaned_data', action='store', type=float, required=True, dest='cleaned_data')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+cleaned_data = args.cleaned_data
+
+
+
+
+def simple_temperature_model(t, base=20, amplitude=5, noise_level=0.5):
+    return base + amplitude * np.sin(t) + np.random.normal(0, noise_level, len(t))
+
+t = np.linspace(0, 20, len(cleaned_data))
+model_output = simple_temperature_model(t)
+
+model_output[:5]
+
+file_model_output = open("/tmp/model_output_" + id + ".json", "w")
+file_model_output.write(json.dumps(model_output))
+file_model_output.close()
