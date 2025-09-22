9d5a0a0ac6fbe8436dea22f3e243ad69041b8425
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-22 19:55:33 +0200

    final-output-json-like-state-summary-dev-localhost creation

---
+++ final-output-json-like-state-summary-dev-localhost/task.py
@@ -0,0 +1,28 @@
+import json
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--digital_twin_state', action='store', type=float, required=True, dest='digital_twin_state')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+digital_twin_state = args.digital_twin_state
+
+
+
+
+
+json_output = json.dumps(digital_twin_state, indent=2, default=str)
+print(json_output)
+
