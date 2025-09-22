bdc5e835b017333a315282773bb487b71e0bc6b2
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-22 19:21:57 +0200

    5-simulate-system-behavior-using-the-model-dev-localhost creation

---
+++ 5-simulate-system-behavior-using-the-model-dev-localhost/task.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--cleaned_data', action='store', type=float, required=True, dest='cleaned_data')
+
+arg_parser.add_argument('--model_output', action='store', type=float, required=True, dest='model_output')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+cleaned_data = args.cleaned_data
+model_output = args.model_output
+
+
+
+
+simulation_df = pd.DataFrame({
+    'timestamp': cleaned_data.index,
+    'simulated_temperature': model_output
+}).set_index('timestamp')
+
+simulation_df.head()
+
+file_simulation_df = open("/tmp/simulation_df_" + id + ".json", "w")
+file_simulation_df.write(json.dumps(simulation_df))
+file_simulation_df.close()
