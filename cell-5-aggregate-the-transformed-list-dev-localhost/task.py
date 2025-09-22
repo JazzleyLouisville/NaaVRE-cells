77aea67295be0c873e83c12bf8b90c9a5f611358
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-22 23:36:47 +0200

    cell-5-aggregate-the-transformed-list-dev-localhost creation

---
+++ cell-5-aggregate-the-transformed-list-dev-localhost/task.py
@@ -0,0 +1,28 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--xs2', action='store', type=str, required=True, dest='xs2')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+xs2 = json.loads(args.xs2)
+
+
+
+s = sum(xs2)
+print('sum(xs2) =', s)
+
+file_s = open("/tmp/s_" + id + ".json", "w")
+file_s.write(json.dumps(s))
+file_s.close()
