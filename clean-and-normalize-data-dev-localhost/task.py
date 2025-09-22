ff0405d6d86afa6cb5fd1e4d2eaa17352a059329
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-22 20:54:38 +0200

    clean-and-normalize-data-dev-localhost update

--- clean-and-normalize-data-dev-localhost/task.py
+++ clean-and-normalize-data-dev-localhost/task.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 import argparse
 import json
@@ -8,7 +9,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--sensor_data', action='store', type=float, required=True, dest='sensor_data')
+arg_parser.add_argument('--json_data', action='store', type=str, required=True, dest='json_data')
 
 
 args = arg_parser.parse_args()
@@ -16,20 +17,18 @@ print(args)
 
 id = args.id
 
-sensor_data = args.sensor_data
+json_data = json.loads(args.json_data)
 
 
 
-cleaned_data = sensor_data.copy()
-
+cleaned_data = pd.read_json(json_data, orient='records')
 cleaned_data = cleaned_data[(cleaned_data['humidity'] >= 20) & (cleaned_data['humidity'] <= 80)]
 
 cleaned_data = cleaned_data.fillna(method='ffill')
 
 cleaned_data['temperature_norm'] = (cleaned_data['temperature'] - cleaned_data['temperature'].mean()) / cleaned_data['temperature'].std()
+cleaned_json_data = cleaned_data.to_json(orient='records', date_format='iso')
 
-cleaned_data.head()
-
-file_cleaned_data = open("/tmp/cleaned_data_" + id + ".json", "w")
-file_cleaned_data.write(json.dumps(cleaned_data))
-file_cleaned_data.close()
+file_cleaned_json_data = open("/tmp/cleaned_json_data_" + id + ".json", "w")
+file_cleaned_json_data.write(json.dumps(cleaned_json_data))
+file_cleaned_json_data.close()
