6c00cfd09fdb7f1944a44b6766494b2b3f00c97f
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-09-23 00:26:50 +0200

    cell-4-transform-a-list-using-the-updated-integer-dev-localhost update

--- cell-4-transform-a-list-using-the-updated-integer-dev-localhost/task.py
+++ cell-4-transform-a-list-using-the-updated-integer-dev-localhost/task.py
@@ -12,8 +12,6 @@ arg_parser.add_argument('--n2', action='store', type=int, required=True, dest='n
 
 arg_parser.add_argument('--xs', action='store', type=str, required=True, dest='xs')
 
-arg_parser.add_argument('--xsq', action='store', type=int, required=True, dest='xsq')
-
 
 args = arg_parser.parse_args()
 print(args)
@@ -22,12 +20,11 @@ id = args.id
 
 n2 = args.n2
 xs = json.loads(args.xs)
-xsq = args.xsq
 
 
 
 xs2 = [x * n2 for x in xs]
-print('xs2 =', xsq)
+print('xs2 =', xs2)
 
 file_xs2 = open("/tmp/xs2_" + id + ".json", "w")
 file_xs2.write(json.dumps(xs2))
