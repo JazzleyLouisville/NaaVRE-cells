05110d54dce20d6d1d9fdf8024465ea60f473d7b
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2025-10-20 19:12:29 +0200

    test-cell1-nutrient-test-user-2

---
+++ test-cell1-nutrient-test-user-2/task.py
@@ -0,0 +1,24 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+P_loads = [1,2,3]
+
+file_P_loads = open("/tmp/P_loads_" + id + ".json", "w")
+file_P_loads.write(json.dumps(P_loads))
+file_P_loads.close()
