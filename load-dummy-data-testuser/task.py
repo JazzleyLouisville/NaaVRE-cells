d706b05f2b46d99b384e573a95fe0e0e736cb3c7
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2026-04-16 22:15:44 +0200

    load-dummy-data-testuser

--- load-dummy-data-testuser/task.py
+++ load-dummy-data-testuser/task.py
@@ -10,11 +10,11 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--filter_point_cloud', action='store', type=None, required=True, dest='filter_point_cloud')
+arg_parser.add_argument('--filter_point_cloud', action='store', type=float, required=True, dest='filter_point_cloud')
 
-arg_parser.add_argument('--np', action='store', type=None, required=True, dest='np')
+arg_parser.add_argument('--np', action='store', type=float, required=True, dest='np')
 
-arg_parser.add_argument('--pd', action='store', type=None, required=True, dest='pd')
+arg_parser.add_argument('--pd', action='store', type=str, required=True, dest='pd')
 
 
 args = arg_parser.parse_args()
@@ -24,7 +24,7 @@ id = args.id
 
 filter_point_cloud = args.filter_point_cloud
 np = args.np
-pd = args.pd
+pd = json.loads(args.pd)
 
 
 
