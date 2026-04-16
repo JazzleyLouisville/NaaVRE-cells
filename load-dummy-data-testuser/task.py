d39c48132c7ad7a0b0cc6e0d6eaa43d1217d0a65
Jazzley Louisville <70904780+JazzleyLouisville@users.noreply.github.com>
2026-04-16 22:18:01 +0200

    load-dummy-data-testuser

--- load-dummy-data-testuser/task.py
+++ load-dummy-data-testuser/task.py
@@ -10,11 +10,11 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--filter_point_cloud', action='store', type=float, required=True, dest='filter_point_cloud')
+arg_parser.add_argument('--filter_point_cloud', action='store', type=None, required=True, dest='filter_point_cloud')
 
-arg_parser.add_argument('--np', action='store', type=float, required=True, dest='np')
+arg_parser.add_argument('--np', action='store', type=None, required=True, dest='np')
 
-arg_parser.add_argument('--pd', action='store', type=str, required=True, dest='pd')
+arg_parser.add_argument('--pd', action='store', type=None, required=True, dest='pd')
 
 
 args = arg_parser.parse_args()
@@ -24,12 +24,12 @@ id = args.id
 
 filter_point_cloud = args.filter_point_cloud
 np = args.np
-pd = json.loads(args.pd)
+pd = args.pd
 
 
 
 df = pd.DataFrame(np.random.rand(100, 4), columns=['x', 'y', 'z', 'intensity'])
 
-processed_data = filter_point_cloud(df, threshold=0.2)
+processed_data = filter_point_cloud(df, threshold=0.3)
 print('Preprocessing complete.')
 
